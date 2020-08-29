718bd3c6304c7e3ff230d6ba50210b62fc322463
Jack <car20593@uvg.edu.gt>
2020-08-28 22:33:56 -0600

    Segunda version de la interfaz donde se agrega la creacion de usuario

--- MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerButton = findViewById(R.id.buttonR);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ MyApplication/app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -0,0 +1,58 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    Button register;
+    EditText user;
+    EditText password;
+    EditText confirmPassword;
+    TextView error;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registar);
+        register = findViewById(R.id.loginBtn2);
+        user =  (EditText) findViewById(R.id.user);
+        password = (EditText) findViewById(R.id.password);
+        confirmPassword = (EditText) findViewById(R.id.confirmPassword);
+        error = findViewById(R.id.error1);
+        register.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                String us = user.getText().toString();
+                String pw = password.getText().toString();
+                String cpw = confirmPassword.getText().toString();
+                if(us.isEmpty()||pw.isEmpty()||cpw.isEmpty()){
+                    error.setText("Llene todos los espacios");
+                    error.setVisibility(View.VISIBLE);
+                }
+                 else {
+                    if(pw.equals(cpw)){
+                        error.setVisibility(View.INVISIBLE);
+                        Intent intent = new Intent(RegisterActivity.this, NotificacionActivity.class);
+                        startActivity(intent);
+
+                    }
+                    else{
+                        error.setVisibility(View.VISIBLE);
+                    }
+                 }
+
+            }
+        });
+    }
+}
