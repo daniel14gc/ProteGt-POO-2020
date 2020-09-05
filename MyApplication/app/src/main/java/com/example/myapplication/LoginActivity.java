ff21316e0325caa3433e425c2385b42bb7d4e8eb
André Asturias <ast20232@uvg.edu.gt>
2020-09-05 13:27:36 -0600

    Se agrego la clase persona e inicio la progamación del controlador

--- MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -6,21 +6,62 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button registerButton;
+    Button loginButton;
+
+    EditText user;
+    EditText password;
+
+    TextView error;
+
+    Driver d = new Driver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        user =  (EditText) findViewById(R.id.user);
+        password = (EditText) findViewById(R.id.password);
+
+        error = findViewById(R.id.errordatos);
 
         registerButton = findViewById(R.id.buttonR);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton = findViewById(R.id.loginBtn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String us = user.getText().toString();
+                String pw = password.getText().toString();
+                if(us.isEmpty()||pw.isEmpty()){
+                    error.setText("Llene todos los espacios");
+                    error.setVisibility(View.VISIBLE);
+                }
+                else {
+
+                    if (d.comparardatos(us, pw)){
+
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
