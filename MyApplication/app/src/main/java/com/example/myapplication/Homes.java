a0c745d449ca4cc241b822af9e7862a643984897
Jack <car20593@uvg.edu.gt>
2020-09-29 14:53:14 -0600

    Creacion de las publicaiones y nueva algunos cambios al Home

--- MyApplication/app/src/main/java/com/example/myapplication/Homes.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Homes.java
@@ -9,13 +9,29 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
 
 public class Homes extends AppCompatActivity {
-
+    ImageView newpost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homes);
+
+        newpost = findViewById(R.id.newpost);
+
+        newpost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Homes.this, Publica.class);
+                startActivity(intent);
+            }
+        });
     }
 }
