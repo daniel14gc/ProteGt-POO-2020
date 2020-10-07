c34b04b82f502e0ee6e840b9513d261545fc3110
Jack <car20593@uvg.edu.gt>
2020-10-07 11:42:35 -0600

    ultimos arreglos de notification, login y privasidad

--- MyApplication/app/src/main/java/com/example/myapplication/NotificacionActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/NotificacionActivity.java
@@ -9,6 +9,15 @@
 
 package com.example.myapplication;
 
+/*-------------------------------------
+Proyecto: ProteGt
+Clase: LoginActivity
+
+Clase que permite mostrar el layout de Notificacion y tiene los metodos
+para que funcione los botones
+
+ -------------------------------------*/
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -21,13 +30,15 @@ import android.widget.Button;
 public class NotificacionActivity extends AppCompatActivity{
 
     Button activarButton;
+
+    //Metodo que indica las acciones que se realizan cuando se crea el Layout.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notificacion);
 
         activarButton = findViewById(R.id.Acept1);
-
+        //Cuando se cliquee el boton de aceptar, se procedera a mover al usuario al Layout de Inicio.
         activarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
