9e11be60392fa0daabcd601c6ea0618c8851c9ea
Daniel14_gc <danielgo.carrillo@gmail.com>
2020-10-24 14:52:18 -0600

    Localización de usuario al abrir mapa

--- MyApplication/app/src/main/java/com/example/myapplication/Map.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Map.java
@@ -9,19 +9,28 @@ package com.example.myapplication;
 
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -29,8 +38,9 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
     private LatLng latLng;
     private ImageView flecha;
     private ImageView regreso;
-
-
+    private Location currentLocation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE = 101;
 
     // Metodo que define las acciones que se realizaran cuando se cree el activity.
     @Override
@@ -51,9 +61,27 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
             }
         });
 
-        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fetchLastLocation();
+    }
 
-        supportMapFragment.getMapAsync(this);
+    public void fetchLastLocation(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location!= null){
+                    currentLocation = location;
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    supportMapFragment.getMapAsync(Map.this);
+                }
+            }
+        });
     }
 
     // Metodo que define las acciones que se realizaran cuando el mapa este listo
@@ -61,9 +89,8 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         gMap = googleMap;
-
-        latLng = new LatLng(14.618988, -90.471653);
-
+        //latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
         gMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
 
             // Este metodo define que acciones se realizaran cuando el mapa se cargue
@@ -82,7 +109,17 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
                 gMap.addMarker(markerOptions);
             }
         });
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_CODE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    fetchLastLocation();
+                }
+                break;
+        }
     }
 
     // Metodo para retornar a homes
