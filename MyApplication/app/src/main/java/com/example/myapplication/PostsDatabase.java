43346528780eff28a7bfa4406a573eedb44ff166
Sebastian Aristondo <ari20880@uvg.edu.gt>
2020-10-25 14:39:37 -0600

    Orden de publicacion

--- MyApplication/app/src/main/java/com/example/myapplication/PostsDatabase.java
+++ MyApplication/app/src/main/java/com/example/myapplication/PostsDatabase.java
@@ -20,6 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class PostsDatabase extends Database {
@@ -67,7 +68,7 @@ public class PostsDatabase extends Database {
 
             }
         });
-
+        Collections.reverse(posts);
         //Se devuelven los posts en un arraylist.
         return posts;
 
