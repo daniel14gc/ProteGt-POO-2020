34c3dff993bcc13dfc51202e264fc32e6a9791a3
Jack <car20593@uvg.edu.gt>
2020-11-04 14:17:52 -0600

    "Se agrego una funcion faltante en la notificaion"

--- MyApplication/app/src/main/java/com/example/myapplication/NotificacionActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/NotificacionActivity.java
@@ -17,10 +17,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class NotificacionActivity extends AppCompatActivity{
 
     private Button activarButton;
+    private TextView msm2;
 
     //Metodo que indica las acciones que se realizan cuando se crea el Layout.
     @Override
@@ -29,8 +31,16 @@ public class NotificacionActivity extends AppCompatActivity{
         setContentView(R.layout.activity_notificacion);
 
         activarButton = findViewById(R.id.Acept1);
+        msm2 = findViewById(R.id.sms2);
         //Cuando se cliquee el boton de aceptar, se procedera a mover al usuario al Layout de Inicio.
         activarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NotificacionActivity.this, Homes.class);
+                startActivity(intent);
+            }
+        });
+        msm2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(NotificacionActivity.this, Homes.class);
